0bb7e85bf48738ffd9489da0fc13d86ec74d047b
Nick Leon <nicolas7@vt.edu>
2021-10-05 18:57:12 -0400

    add routes for html files

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose")
+const logger = require("morgan");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+// app.use(require("./routes/api.js"));
+app.get("/exercise", (req, res) => {
+    res.sendFile(__dirname + "/public/exercise.html");
+  });
+
+app.get("/stats", (req, res) => {
+    res.sendFile(__dirname + "/public/stats.html");
+  });
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
